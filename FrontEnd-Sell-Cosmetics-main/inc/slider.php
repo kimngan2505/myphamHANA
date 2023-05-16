b95078b71d76bd547eb919bcacbae770ab8959ee
khucthuan <thuan2015vl@gmail.com>
2023-05-16 18:33:20 +0700

    5/16/2023

---
+++ FrontEnd-Sell-Cosmetics-main/inc/slider.php
@@ -0,0 +1,122 @@
+  <!-- Menu -->
+  <div class="header__nav">
+            <ul class="header__nav-list">
+                <li class="header__nav-item nav__search">
+                    <div class="nav__search-wrap">
+                        <input class="nav__search-input" type="text" name="" id="" placeholder="Tìm sản phẩm...">
+                    </div>
+                    <div class="nav__search-btn">
+                        <i class="fas fa-search"></i>
+                    </div>
+                </li>
+                <li class="header__nav-item authen-form">
+                    <a href="#" class="header__nav-link">Tài Khoản</a>
+                    <ul class="sub-nav">
+                        <li class="sub-nav__item">
+                            <a href="#my-Login" class="sub-nav__link">Đăng Nhập</a>
+                        </li>
+                        <li class="sub-nav__item">
+                            <a href="#my-Register" class="sub-nav__link">Đăng Kí</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="header__nav-item index">
+                    <a href="index.php" class="header__nav-link">Trang chủ</a>
+                </li>
+                <li class="header__nav-item">
+                    <a href="#" class="header__nav-link">Giới Thiệu</a>
+                </li>
+                <li class="header__nav-item">
+                    <a href="#" class="header__nav-link">Sản Phẩm</a>
+                    <div class="sub-nav-wrap grid wide">
+                        <ul class="sub-nav">
+                            <li class="sub-nav__item">
+                                <a href="" class="sub-nav__link heading">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc toàn thân vvv</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Khuyến mãi</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc cơ thể</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc miệng</a>
+                            </li>
+                        </ul>
+                        <ul class="sub-nav">
+                            <li class="sub-nav__item">
+                                <a href="" class="sub-nav__link heading">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc toàn thân vvv</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Khuyến mãi</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc cơ thể</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc miệng</a>
+                            </li>
+                        </ul>
+                        <ul class="sub-nav">
+                            <li class="sub-nav__item">
+                                <a href="" class="sub-nav__link heading">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc toàn thân vvv</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Khuyến mãi</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc cơ thể</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc miệng</a>
+                            </li>
+                        </ul>
+                        <ul class="sub-nav">
+                            <li class="sub-nav__item">
+                                <a href="" class="sub-nav__link heading">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc toàn thân vvv</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Khuyến mãi</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc cơ thể</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Nước hoa</a>
+                            </li>
+                            <li class="sub-nav__item">
+                                <a href="listProduct.php" class="sub-nav__link">Chăm sóc miệng</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="header__nav-item">
+                    <a href="news.php" class="header__nav-link">Tin Tức</a>
+                </li>
+                <li class="header__nav-item">
+                    <a href="contact.php" class="header__nav-link">Liên Hệ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
---
+++ FrontEnd-Sell-Cosmetics-main/pay.php
@@ -0,0 +1,94 @@
+<?php
+include 'inc/headerPay.php';
+include 'inc/slider.php';
+?>
+    <div class="main">
+        <div class="grid wide">
+            <div class="row">
+                <div class="col l-7 m-12 s-12">
+                    <div class="pay-information">
+                        <div class="pay__heading">Thông tin thanh toán</div>
+                        <div class="form-group">
+                            <label for="account" class="form-label">Họ Tên *</label>
+                            <input id="account" name="account" type="text" class="form-control">
+                            <span class="form-message">Không hợp lệ !</span>
+                        </div>
+                        <div class="form-group">
+                            <label for="account" class="form-label">Địa chỉ *</label>
+                            <input id="account" name="account" type="text" class="form-control">
+                            <span class="form-message"></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="account" class="form-label">Tỉnh / Thành phố *</label>
+                            <input id="account" name="account" type="text" class="form-control">
+                            <span class="form-message"></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="account" class="form-label">Email *</label>
+                            <input id="account" name="account" type="text" class="form-control">
+                            <span class="form-message"></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="account" class="form-label">Số điện thoại *</label>
+                            <input id="account" name="account" type="text" class="form-control">
+                            <span class="form-message"></span>
+                        </div>
+                        <div class="form-group">
+                            <label for="account" class="form-label">Ghi chú cho đơn hàng</label>
+                            <textarea class="form-control" name="" id="" cols="30" rows="20"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="col l-5 m-12 s-12">
+                    <div class="pay-order">
+                        <div class="pay__heading">Đơn hàng của bạn</div>
+                        <div class="pay-info">
+                            <div class="main__pay-text">
+                                Azrouel dress variable</div>
+                            <div class="main__pay-price">
+                                1,120,000 ₫
+                            </div>
+                        </div>
+                        <div class="pay-info">
+                            <div class="main__pay-text">
+                                Azrouel dress variable </div>
+                            <div class="main__pay-amount">
+                                3
+                            </div>
+                            <div class="main__pay-price">
+                                1,120,000 ₫
+                            </div>
+                        </div>
+                        <div class="pay-info">
+                            <div class="main__pay-text">
+                                Azrouel dress variable </div>
+                            <div class="main__pay-price">
+                                1,120,000 ₫
+                            </div>
+                        </div>
+                        <div class="pay-info">
+                            <div class="main__pay-text special">
+                                Giao hàng
+                            </div>
+                            <div class="main__pay-text">
+                                Giao hàng miễn phí
+                            </div>
+
+                        </div>
+                        <div class="pay-info">
+                            <div class="main__pay-text special">
+                                Tổng thành tiền</div>
+                            <div class="main__pay-price">
+                                1,120,000 ₫
+                            </div>
+                        </div>
+                        <div class="btn btn--default">Đặt hàng</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+ <?php
+include 'inc/footer.php';
+
+?>
