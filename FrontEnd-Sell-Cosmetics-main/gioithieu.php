fae6eb0e5804dcb0827c7166bc545f59ac05ffb0
kimngan2505 <nganb1906328@student.ctu.edu.vn>
2023-05-17 15:10:12 +0700

    Trang giới thiệu

---
+++ FrontEnd-Sell-Cosmetics-main/gioithieu.php
@@ -0,0 +1,47 @@
+<?php
+include 'inc/header.php';
+?>
+    <div class="main">
+        <div class="grid wide">
+            <div class="main__taskbar">
+                <div class="main__breadcrumb">
+                    <div class="breadcrumb__item">
+                        <a href="#" class="breadcrumb__link">Trang chủ</a>
+                    </div>
+                    <div class="breadcrumb__item">
+                        <a href="#" class="breadcrumb__link">Giới thiệu về cửa hàng</a>
+                    </div>
+                </div>
+            </div>
+            <div class="list-new">
+                <div href="#" class="new-item">
+                    <a href="#" class="new-item__img">
+                        <img src="./image/logonew.png" alt="">
+                    </a>
+                    
+                    <div class="new-item__body">
+                        <a href="#" class="new-item__title">
+                            Cửa hàng mỹ phẩm HANA thương hiệu Việt - Chất lượng Việt
+                       </a>
+                        <p class="new-item__time"> Ngày đăng: 27/5/2023</p>
+                        <p class="new-item__description">Một lựa chọn đáng tin cậy khác khi bạn muốn mua mỹ phẩm chính hãng đó là website myphamhana.vn. Tại đây có đa dạng các loại mỹ phẩm làm đẹp từ chăm sóc cơ thể, chăm sóc khuôn mặt, các dòng kem chống nắng, son môi, mặt nạ, dưỡng ẩm,… đến từ nhiều thương hiệu uy tín trong và ngoài nước.
+                            Điểm nổi bật ở myphamhana chính là không chỉ đảm bảo 100% mỹ phẩm chính hãng, chất lượng mà còn làm chúng ta choáng ngợp bởi con số mỹ phẩm được bày bán. Chẳng hạn, với sản phẩm kem chống nắng, bạn sẽ tìm được đầy đủ loại dành cho da khô, da dầu,… đến từ thương hiệu Hàn Quốc, Nhật Bản, Anh,… </p>
+                        <p class="new-item__description">Điều này mang đến cho chúng ta nhiều sự lựa chọn tối ưu và mở mang thêm về thị trường mỹ phẩm làm đẹp hiện nay.
+                            Chọn mua mỹ phẩm tại myphamhana bạn có thể tìm được giá tốt nhất vì myphamhana có tính năng so sánh giá, tức là tổng hợp nhiều mức giá được bán trên các sàn thương mại điện tử nổi tiếng, cùng kho voucher, coupon lên đến 50%, rất ưu đãi ngập tràn, giá rẻ bất ngờ đấy.
+                            Điểm cộng lớn nhất ở myphamhana chính là giao diện thông minh, dễ sử dụng cùng các bài đánh giá về mỹ phẩm có tâm và chỉnh chu. Bạn hầu như tìm được những thông tin cần thiết trước khi chọn mua mà không cần tham khảo bất cứ nguồn tin nào khác.</p>
+                            <div class="avatar">
+                                <img src="./image/new.png" width="35px"> Huỳnh Mỹ Anh
+                                <p>Nhận xét từ khách hàng: ngày 21/05/2023</p>
+                            </div>    
+                            
+                            <p class="new-item__description">Lúc đầu mình khá băn khoăn khi vào website này trải nghiệm, nhưng mình rất bất ngờ vì ngoài giúp mình mua hàng chính hãng với giá rẻ, ở đây còn có vô vàn bài chia sẻ về cách dưỡng da, cách sử dụng mỹ phẩm chuẩn, các thành phần nào hay dùng trong mỹ phẩm. 
+                            Với mình đây như là một kênh thông tin lý thú để bổ sung kiến thức hàng ngày. Được nhiều đánh giá tích cực, mình nghĩ đây là một nơi mua hàng rất nên thử, cũng là nơi gom nhặt thêm nhiều thông tin hữu dụng trong cuộc sống đấy!</p>
+                        <img src="./image/cua-hang-my-pham.png" alt="">
+                            <p class="new-item__description">Hình ảnh khách hàng mua mỹ phẩm tại BEAUTY HANA</p>
+                            <div class="muahang">
+                            &nbsp;&nbsp; <img src ="./image/khach-hang-mua-my-pham.jpg" width="320px"> <img src ="./image/khach-hang-mua-my-pham2.jpg" width="355px"> <img src ="./image/cách-chào-hàng-mỹ-phẩm-online.jpg" width="308px">
+                            </div>
+                        <p class="new-item__description">Trải nghiệm ngay mua mỹ phẩm tại BEAUTY HANA !!!</p>
+                        <a href="index.php" class="btn btn--default">Trang chủ</a>
+                    </div>
+                </div>
